32f49bac94e6b9d84e3176291eae7b4dd84b4f2c
Халяпин Вадим <vadim.halyapin@yandex.ru>
2020-09-23 11:25:28 +0300

    Добавьте файлы проекта.

---
+++ InsertedIfOperator2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Programm
+{
+    static void Main()
+    {
+        int number;
+        Console.Write("ВВедите целое число:");
+        number = Int32.Parse(Console.ReadLine());
+        if (number == 1)
+            Console.WriteLine("Адын");
+        else if (number == 2)
+            Console.WriteLine("Два");
+         else if (number == 3)
+            Console.WriteLine("Три");
+         else if (number == 4)
+            Console.WriteLine("Четыре");
+         else
+            Console.WriteLine("Хз");
+      
+    }
+}
